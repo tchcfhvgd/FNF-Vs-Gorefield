270b52a945d1a0e7b47441f31276e68b6a92d1e8
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-16 09:58:52 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -73,7 +73,7 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
-		ClientPrefs.startControls();
+		ClientPre
 		
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
