f00fdc8a0527fa86b92860e29473b41d51357943
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-11-12 12:51:23 +0800

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -114,7 +114,7 @@ class GameOverSubstate extends MusicBeatSubstate
 			PlayState.seenCutscene = false;
 
 			if (PlayState.isStoryMode)
-				MusicBeatState.switchState(new StoryMenuState());
+				MusicBeatState.switchState(new MainMenuState());
 			else
 				MusicBeatState.switchState(new FreeplayState());
 
